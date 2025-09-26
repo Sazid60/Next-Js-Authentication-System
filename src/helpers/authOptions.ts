0535bbd461d835b4a08f098c83d86c3b225335dc
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-09-26 10:07:36 +0600

    54-3 Access session user & logout safely

--- src/helpers/authOptions.ts
+++ src/helpers/authOptions.ts
@@ -7,7 +7,7 @@ export const authOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
         })
-
     ],
+    secret : process.env.AUTH_SECRET
 }
 
